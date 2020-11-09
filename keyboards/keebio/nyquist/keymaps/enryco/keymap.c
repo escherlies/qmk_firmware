40b6acd98442f0d6863c95e57f42e37e842ad2e3
Enrico Scherlies <e.scherlies@me.com>
2020-11-09 13:57:27 +0100

    ergonomic and functional changes

--- keyboards/keebio/nyquist/keymaps/enryco/keymap.c
+++ keyboards/keebio/nyquist/keymaps/enryco/keymap.c
@@ -77,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     esc         , KC_1, KC_2, KC_3, KC_4 , KC_5, KC_6 , KC_7 , KC_8, KC_9, KC_0, bspc,
     tab         , KC_Q, KC_W, KC_F, KC_P , KC_B, KC_J , KC_L , KC_U, KC_Y, scln, lbrc,
     bspc        , KC_A, KC_R, KC_S, KC_T , KC_G, KC_M , KC_N , KC_E, KC_I, KC_O, ent,
-    LSFT_T(caps), KC_Z, KC_X, KC_C, KC_D , KC_V, KC_K , KC_H , comm, dot , slsh, rsft,
-    MO(5)       , lctl, lalt, lgui, MO(4), spc , MO(3), MO(4), left, down, up  , rght
+    lsft        , KC_Z, KC_X, KC_C, KC_D , KC_V, KC_K , KC_H , comm, dot , slsh, rsft,
+    MO(5)       , lctl, ____, lalt, lgui, spc , MO(3), MO(4), left, down, up  , rght
   ),
 
   [1] = LAYOUT_ortho_5x12(
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ____, ____, ____, ____, ____,  ____, esc , home, up  , pgup, ins , ____,
     ____, ____, ____, ____, ____,  ____, bspc, left, down, rght, tab , ____,
     ____, ____, ____, ____, ____,  ____, ____, end , ____, pgdn, del , ____,
-    ____, ____, ____, ____, MO(4), ____, ____, ____, mply, vold, volu, mnxt
+    ____, ____, ____, ____, ____, ____, ____, ____, mply, vold, volu, mnxt
   ),
 
   [4] = LAYOUT_ortho_5x12(
@@ -110,14 +110,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ____, at  , perc, plus, astr , circ, ____, lbrc, ampr, unds, rbrc, ____,
     ____, lt  , mins, eql , gt   , hash, pipe, lprn, quot, coln, rprn, ____,
     ____, grv , tild, slsh, exlm , bsls, dlr , lcbr, dquo, eql , rcbr, ____,
-    ____, ____, ____, ____, MO(3), ____, ____, ____, ____, ____, ____, ____
+    ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____
   ),
 
   [5] = LAYOUT_ortho_5x12(
     KC_F12, KC_F1, KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10, KC_F11,
     xxxx  , RESET, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, xxxx  , del,
     xxxx  , xxxx , xxxx   , AU_ON  , AU_OFF , AG_NORM, AG_SWAP, TO(0)  , TO(1)  , TO(2)  , xxxx  , xxxx,
-    xxxx  , xxxx , xxxx   , xxxx   , xxxx   , xxxx   , xxxx   , DF(0)  , DF(1)  , xxxx   , xxxx  , xxxx,
+    xxxx  , xxxx , xxxx   , xxxx   , xxxx   , xxxx   , xxxx   , DF(0)  , DF(1)  , xxxx   , xxxx  , KC_CAPS,
     xxxx  , xxxx , xxxx   , xxxx   , xxxx   , xxxx   , xxxx   , xxxx   , xxxx   , xxxx   , xxxx  , xxxx
   ),
 };
